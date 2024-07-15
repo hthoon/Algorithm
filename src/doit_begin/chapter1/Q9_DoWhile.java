3b1b36d561015bef651aed2c7e60181eaacbbf19
thoon <winwin_th@naver.com>
2024-07-15 20:55:11 +0900

    Do it 챕터1 Q1 ~ Q8

---
+++ src/doit_begin/chapter1/Q9_DoWhile.java
@@ -0,0 +1,32 @@
+package doit_begin.chapter1;
+
+import java.util.Scanner;
+
+public class Q9_DoWhile {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b;
+        int result = 0;
+
+        do {
+            System.out.println("a 입력: ");
+            a = input.nextInt();
+            System.out.println("b 입력: ");
+            b = input.nextInt();
+        }
+        while (a < b);
+
+
+
+    }
+
+    static int DoWhile(int a, int b) {
+
+
+
+
+        return result;
+    }
+
+}
