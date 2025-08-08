e7c7b6430e6a837c4a526bad227b7ce6e81e6197
thoon <winwin_th@naver.com>
2025-08-08 17:13:17 +0900

    Java 답안 템플릿

---
+++ src/baekjoon/java/JavaTemplate.java
@@ -0,0 +1,14 @@
+package baekjoon.java;
+
+import java.io.*;
+
+public class JavaTemplate {
+    /**
+     *
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
