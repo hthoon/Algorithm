11a380965b0eadc8abbbfae2dc2b6735a8ebc74c
thoon <winwin_th@naver.com>
2025-08-17 22:28:23 +0900

    백준 - S4: 1920 - 수 찾기, 문제 의도에 맞게 다시 풀이(Java)

--- src/baekjoon/java/solved_class/class2/essential/N1920.java
+++ src/baekjoon/java/solved_class/class2/essential/N1920.java
@@ -1,46 +1,63 @@
 package baekjoon.java.solved_class.class2.essential;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class N1920 {
     /**
-     * 수 찾기 S4
+     * 수 찾기 - 이분 탐색
      * */
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
-        /* 정수 Set */
-        Set<Long> nums = new HashSet<>();
-
-        /* Set에 정수 입력 */
+        /* 정수 배열 */
+        int[] nums = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            nums.add(Long.parseLong(st.nextToken()));
+            nums[i] = Integer.parseInt(st.nextToken());
         }
 
-        /* 검사 대상 정수 */
+        // 이분 탐색을 위해 정렬
+        Arrays.sort(nums);
+
+        /* 탐색 대상 정수 배열 */
         int M = Integer.parseInt(br.readLine());
-
-        /* 존재 여부에 따라 출력
-        -> StringBuilder 사용 시 메모리, 시간 이점 큼
-        */
-
+        int[] targets = new int[M];
         st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            targets[i] = Integer.parseInt(st.nextToken());
+        }
+
+        /* 탐색 */
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
-            if (nums.contains(Long.parseLong(st.nextToken()))) {
-                sb.append(1).append("\n");
-            }
-            else sb.append(0).append("\n");
+            sb.append(bs(nums, targets[i])).append("\n");
         }
-        System.out.println(sb);
+        System.out.print(sb);
+    }
+
+    private static int bs(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // 탐색 성공
+            if (nums[mid] == target) {
+                return 1;
+            }
+            // 탐색 범위가 대상보다 클 경우,
+            else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+            // 탐색 범위가 대상보다 작을 경우,
+            else {
+                left = mid + 1;
+            }
+        }
+        return 0;
     }
 }
